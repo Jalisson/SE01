00d60772e0bb6b104c8a78f64984984e8e2f6636
Jalisson <jal_neves@hotmail.com>
2021-12-16 17:24:07 -0300

    ImplemnetaçãoBootStrep

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('conteudo')
+    
+<div class="menu">
+    <img src="" alt="" sizes="" srcset="">  
+    <ul>
+        <a href=""><li>Deachboard</li></a>
+        <a href=""><li>Area do Aluno</li></a>
+        <a href=""><li>Area do Professor</li></a>
+        <a href=""> <li>Area Fianceira</li></a>
+        <a href=""><li>Area Administrativa</li></a>
+    </ul>
+</div>
+    
+@endsection
